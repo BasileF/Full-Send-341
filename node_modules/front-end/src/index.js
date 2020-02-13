dabafb143609ba12c69f252877de9c0deecd7c74
Michael Arabian <Michael>
2020-02-13 15:28:37 -0500

    added boolean funtion to button component1

---
+++ node_modules/front-end/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import followComponent from './FollowComponent/followComponent';
+
+
+ReactDOM.render(<followComponent/>, document.getElementById('root'));
