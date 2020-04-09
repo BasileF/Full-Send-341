9b8359e2445abe2b84b9f29788745f8114562700
Michael Arabian <michael-nareg99@hotmail.com>
2020-04-08 21:29:38 -0400

    Michael/Corona 2

---
+++ front-end/src/components/fullsendBanner/detection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const predictor = new PredictionApi.PredictionAPIClient(predictionKey, endPoint);
+    const testFile = fs.readFileSync(`${sampleDataRoot}/Test/test_od_image.jpg`);
+
+    const results = await predictor.detectImage(sampleProject.id, publishIterationName, testFile)
+
+    // Show results
+    console.log("Results:");
+    results.predictions.forEach(predictedResult => {
+        console.log(`\t ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}% ${predictedResult.boundingBox.left},${predictedResult.boundingBox.top},${predictedResult.boundingBox.width},${predictedResult.boundingBox.height}`);
+    });
+})()
