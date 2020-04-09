863a0f4badf122ee3682ca073a0477870d3fa674
Michael Arabian <michael-nareg99@hotmail.com>
2020-04-08 21:13:34 -0400

    Corona Detector

--- front-end/src/components/landing/LoggedIn.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CreatePostPage from '../posting/CreatePostPage';
-
-export default function LoggedIn() {
-  return (
-    <CreatePostPage />
-  );
-}
--- front-end/src/interfaces/common.ts
+++
@@ -1,5 +0,0 @@
-export interface FirebaseRequirements {
-  auth: firebase.auth.Auth;
-  db: firebase.database.Database;
-  storage: firebase.storage.Storage;
-}
